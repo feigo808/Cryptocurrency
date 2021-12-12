25585dcb7a4176134852f62b073c993d8bf2662d
feigo808 <feigo.code@gmail.com>
2021-12-12 00:13:12 -1000

    init commit, import ui theme

---
+++ app/src/main/java/com/feiyatsu/cryptocurrency/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.feiyatsu.cryptocurrency
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.feiyatsu.cryptocurrency.ui.theme.CryptocurrencyAppYTTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptocurrencyAppYTTheme {
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
